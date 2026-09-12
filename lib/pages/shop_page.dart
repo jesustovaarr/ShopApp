802930c70fb4b0dd8e0ec1f83c57b631c2e0ca69
Jesus Elguera <jesustovaarr@gmail.com>
2026-09-12 17:25:31 -0600

    feat: Sneaker Shop
    Sneaker shopping app based on a YouTube tutorial.
    
    Credits:
    https://www.youtube.com/watch?v=UcwsuZP071Y

---
+++ lib/pages/shop_page.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../components/shoe_tile.dart';
+import '../models/cart.dart';
+import '../models/shoe.dart';
+
+class ShopPage extends StatefulWidget {
+  const ShopPage({super.key});
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  // add shoe to cart
+  void addShoeToCart(Shoe shoe) {
+    Provider.of<Cart>(context, listen: false).addItemToCart(shoe);
+
+    // alert the user, shoe successfully added
+    showDialog(
+      context: context,
+      builder: (context) => const AlertDialog(
+        title: Text('Successfully added!'),
+        content: Text('Check your cart'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Cart>(
+      builder: (context, value, child) => Column(
+        children: [
+          // search bar
+          Container(
+            padding: const EdgeInsets.all(12),
+            margin: const EdgeInsets.symmetric(horizontal: 25),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8),
+            ), // BoxDecoration
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Search',
+                  style: TextStyle(color: Colors.grey),
+                ), // Text
+                Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ), // Icon
+              ],
+            ), // Row
+          ), // Container
+
+          // message
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25.0),
+            child: Text(
+              'everyone flies.. some fly longer than others',
+              style: TextStyle(color: Colors.grey[600]),
+            ), // Text
+          ), // Padding
+
+          // hot picks
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: const [
+                Text(
+                  'Hot Picks 🔥',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ), // Text
+                Text(
+                  'See all',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ), // Text
+              ],
+            ), // Row
+          ), // Padding
+
+          const SizedBox(height: 10),
+
+          // list of shoes for sale
+          Expanded(
+            child: ListView.builder(
+              itemCount: 4,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                // get a shoe from shop list
+                Shoe shoe = value.getShoeList()[index];
+
+                // return the shoe
+                return ShoeTile(
+                  shoe: shoe,
+                  onTap: () => addShoeToCart(shoe),
+                ); // ShoeTile
+              },
+            ), // ListView.builder
+          ), // Expanded
+
+          const Padding(
+            padding: EdgeInsets.only(top: 25.0, left: 25, right: 25),
+            child: Divider(
+              color: Colors.white,
+            ), // Divider
+          ), // Padding
+        ],
+      ), // Column
+    ); // Consumer
+  }
+}
